85c0f48e213a2e2e3628fb0931056d36ba7d7234
laguto <Nathan.wigley@hotmail.com>
2024-04-18 17:07:19 +0800

    should be final changes

--- src/main/java/edu/curtin/app/interfaces/EstimationStrategy.java
+++ src/main/java/edu/curtin/app/interfaces/EstimationStrategy.java
@@ -1,6 +1,11 @@
 package edu.curtin.app.interfaces;
 import java.util.*;
 
+/*
+  Interface for the estimation strategy selected by the configuration.
+  This is a part of the implementation of the strategy pattern
+ */
+
 public interface EstimationStrategy {
     int estimate(List<Integer> estimates);
 }
